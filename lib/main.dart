6ae40d3650dd46cb1f38c7a7120173bf6df50c36
hanadaUG <hanadaUG@gmail.com>
2022-07-10 00:08:10 +0900

    rename SlidingPuzzle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SlidingPuzzle());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SlidingPuzzle extends StatelessWidget {
+  const SlidingPuzzle({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
